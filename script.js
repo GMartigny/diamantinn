860611a6131dede60b4de786f0e3a9b7149a4a54
Guillaume Martigny <guillaume.martigny@gmail.com>
2023-09-15 18:32:51 +0200

    Restart from scratch with tailwind

---
+++ script.js
@@ -0,0 +1,88 @@
+window.addEventListener("scroll", () => {
+    document.body.classList.toggle("scrolled", window.scrollY > 0);
+}, {
+    passive: true,
+});
+
+window.addEventListener("load", async () => {
+    // await wait(1000);
+    document.body.classList.add("loaded");
+})
+
+const splashPics = [
+    "pool",
+    "terrace",
+    "bed",
+    "drink",
+];
+
+const container = document.getElementById("splash");
+const fader = document.createElement("div");
+const fadeDuration = 1000;
+fader.className = `bg-full transition-opacity duration-1000 opacity-0`;
+container.appendChild(fader);
+let currentPic = -1;
+
+const cache = {};
+function load (src) {
+    if (cache[src]) {
+        return cache[src];
+    }
+    const img = new Image();
+    img.src = src;
+
+    const promise = new Promise((resolve, reject) => {
+        img.addEventListener("load", () => resolve(img));
+        img.addEventListener("error", reject);
+    });
+
+    cache[src] = promise;
+
+    return promise;
+}
+
+function wait (time) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
+}
+
+async function next (time = 0) {
+    currentPic = (currentPic + 1) % splashPics.length;
+
+    const [img] = await Promise.all([
+        load(`./media/${splashPics[currentPic]}.webp`),
+        wait(time),
+    ]);
+
+    fader.style.backgroundImage = `url(${img.src})`;
+    requestAnimationFrame(async () => {
+        fader.style.opacity = "1";
+
+        await wait(fadeDuration);
+
+        container.style.backgroundImage = fader.style.backgroundImage;
+        fader.style.opacity = "0";
+
+        next(3000);
+    });
+}
+next();
+
+const nav = document.getElementById("nav");
+document.querySelectorAll(".anchor")
+    .forEach(anchor => {
+        anchor.id = anchor.textContent
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/ /g, "_")
+            .replace(/\W/g, "");
+        const li = document.createElement("li");
+        li.className = "flex";
+        const a = document.createElement("a");
+        a.href = `#${anchor.id}`;
+        a.textContent = anchor.textContent;
+        a.className = "p-4 transition-all hover:underline hover:bg-gray-950";
+        li.appendChild(a);
+        nav.appendChild(li);
+    });
